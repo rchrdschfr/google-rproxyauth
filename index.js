1203ad9c081f7e893b173ecefd1e0f6b279670e7
Richard Schaefer <richard@Richards-MacBook-Air.local>
2015-11-06 18:25:16 +0900

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+  var domain = "localhost:8000";
+  var subdomain = req.headers.host.replace("." + domain, "");
+
+  proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    proxyReq.setHeader('X-Forwarded-Host', req.headers.host);
+    proxyReq.setHeader('X-Forwarded-UserEmail', new Date());
+  });
+  proxy.web(req, res, { target: 'http://' + subdomain });
+}).listen(8000);
---
+++ readme.md
@@ -0,0 +1,12 @@
+How to use:
+
+`npm install`
+`node index.js`
+
+Then go to domainYouWantToProxy.localhost:8000
+
+I have a WP site deployed to Digital Ocean, IP = 159.203.132.145.
+
+So, you can proxy it by going to
+
+159.203.132.145.localhost:8000
